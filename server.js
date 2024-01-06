c0cf6f43c63eb6e1ab1b6f9759a37c7c3e065e30
Saicharan Gouru <gourusaicharan6@gmail.com>
2024-01-06 14:09:02 +0530

    commit from terminal

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
-console.log("Hello")
+const express = require('express');
+const app = express()
+
+
+app.get("/", (req, res) => {
+    res.send("Hello")
+})
+
+app.listen(5000, () => console.log("Listening"))
